e749361297c9db9daa1a8f1348b11711afb2e21d
hugolaib14 <hugolaib14@alumnes.ilerna.com>
2026-03-02 15:44:38 +0100

    Push sobre cambios en el menu de pantalla de la Partida.
    Cambios agregados a la clase de Menu Pantalla Partida.

--- src/Menu/PantallaPartida.java
+++ src/Menu/PantallaPartida.java
@@ -6,8 +6,16 @@ public class PantallaPartida {
     public void botonTirarDado() { System.out.println("Dado lanzado"); }
     public void botonUsarObjeto() { System.out.println("Objeto usado"); }
     public void botonFinalizarTurno() { System.out.println("Turno finalizado"); }
-    public void iniciarPartida() {}
-    public void cargarPartida() {}
-    public void guardarPartida() {}
-    public void refrescarPartida() {}
+    public void iniciarPartida() {
+    	
+    }
+    public void cargarPartida() {
+    	
+    }
+    public void guardarPartida() {
+    	
+    }
+    public void refrescarPartida() {
+    	
+    }
 }
