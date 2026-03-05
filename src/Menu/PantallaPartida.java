8a90db34ec7c01dee0b3779c3d608f95335509f5
MartiBadia <badia.bernaduca@alumnes.ilerna.com>
2026-03-05 17:32:08 +0100

    commit correccion
    correccion de clases de vista. (no tiene que haber nada)

--- src/Menu/PantallaPartida.java
+++ src/Menu/PantallaPartida.java
@@ -1,21 +1,6 @@
-package Menu;
+package menu;
 
 public class PantallaPartida {
-    public PantallaPartida() {}
-
-    public void botonTirarDado() { System.out.println("Dado lanzado"); }
-    public void botonUsarObjeto() { System.out.println("Objeto usado"); }
-    public void botonFinalizarTurno() { System.out.println("Turno finalizado"); }
-    public void iniciarPartida() {
-    	
+    public PantallaPartida() {
+    	}
     }
-    public void cargarPartida() {
-    	
-    }
-    public void guardarPartida() {
-    	
-    }
-    public void refrescarPartida() {
-    	
-    }
-}
