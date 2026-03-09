db059d4aa2c5ebb096e47a02d5fb072ab661ae42
MartiBadia <badia.bernaduca@alumnes.ilerna.com>
2026-03-09 20:59:49 +0100

    Commit modificaciones
    Modificaciones a varias clases

--- src/Tablero/Casilla.java
+++ src/Tablero/Casilla.java
@@ -1,12 +1,33 @@
-package Tablero;
+package tablero;
 
-import Partida.Partida;
-import Jugador.Jugador;
+import jugador.Jugador;
+import partida.Partida;
 
 public abstract class Casilla {
     protected int posicion; // Cambiado a protected para acceso directo
 
-    public Casilla() {} // Constructor vacío para evitar el uso de super
+    // Constructor vacío
+    public Casilla() {}
+
+    // Constructor con posición
+    public Casilla(int posicion) {
+        this.posicion = posicion;
+    }
+
+    // --- GETTER ---
+    public int getPosicion() {
+        return posicion;
+    }
+
+    // --- SETTER ---
+    public void setPosicion(int posicion) {
+        this.posicion = posicion;
+    }
 
     public abstract void realizarAccion(Partida partida, Jugador jugador);
+
+    @Override
+    public String toString() {
+        return "Casilla " + posicion + " [" + getClass().getSimpleName() + "]";
+    }
 }
--- src/Tablero/Oso.java
+++ src/Tablero/Oso.java
@@ -1,13 +1,22 @@
-package Tablero;
-import Partida.Partida;
-import Jugador.Jugador;
+package tablero;
+import jugador.Jugador;
+import partida.Partida;
 
 public class Oso extends Casilla {
+
+    // Constructor
     public Oso(int pos) {
-        this.posicion = pos; // Asignación directa sin super()
+        this.posicion = pos;
     }
-    
+
+    @Override
     public void realizarAccion(Partida p, Jugador j) {
-        j.moverPosicion(-2);
+        // Según el enunciado: "si un jugador es atacado, retorna al inicio del juego"
+        j.setPosicion(0);
+    }
+
+    @Override
+    public String toString() {
+        return "Casilla Oso en posicion " + posicion;
     }
 }
